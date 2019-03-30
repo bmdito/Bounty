1f71539458cf6a6f1b063476c7b066c0f7256dd7
Lap Larry Nguyen <larrylapnguyen@gmail.com>
2019-03-30 13:54:51 -0700

    Lint fix

--- models/example.js
+++ models/example.js
@@ -1,31 +1,31 @@
 module.exports = function(sequelize, DataTypes) {
-	var Bounty = sequelize.define("Bounty", {
-	  title: {
-		type: DataTypes.TEXT,
-		allowNull: false,
-		validate: {
-		  len: [1, 100]
-		}
-	  },
-	  description: {
-		type: DataTypes.TEXT,
-		allowNull: false,
-		validate: {
-		  len: [1, 500]
-		}
-	  },
-	  payment: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-		validate: {
-		  min: 0
-		}
-	  },
-	  completed: {
-		type: DataTypes.BOOLEAN,
-		allowNull: false,
-		defaultValue: false
-	  }
-	});
-	return Bounty;
-  };
+  var Bounty = sequelize.define("Bounty", {
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 100]
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 500]
+      }
+    },
+    payment: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 0
+      }
+    },
+    completed: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  });
+  return Bounty;
+};
