c8820d8af3e3e63c586fcfb28f179c598c620f2c
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-04-08 13:43:17 -0700

    updated landing page, added front end val

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -30,7 +30,34 @@ module.exports = function(app) {
 
   // Create a new Bounty
   app.post("/add_bounty", function(req, res) {
-		db.Bounty.create({
+
+  let { title, description, payment } = req.body;
+  let errors = [];
+  
+  //collects errors for missing fields
+  if(!title){
+    errors.push({ text: "please add title!"});
+  }
+
+  if(!description){
+    errors.push({ text: "please add a Bounty description!"});
+  }
+
+  if(!payment){
+    errors.push({ text: "please add a Reward!"});
+  }
+
+  if(errors.length>0){
+    res.render('add', {
+        errors,
+        title,
+        description,
+        payment      
+    
+    });
+  } else {
+
+    db.Bounty.create({
 		  title: req.body.title,
 		  description: req.body.description,
 		  payment: req.body.payment,
@@ -38,6 +65,7 @@ module.exports = function(app) {
 	
 		}).then(function() {
 		res.redirect('/bounties');	  
-		});
+    });
+  }    
 	  });
 };
