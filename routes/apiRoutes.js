04953df9a78eef13020159475d7d130915b9c12a
Lap Larry Nguyen <larrylapnguyen@gmail.com>
2019-04-08 13:23:56 -0700

    adds audio/grid/more styling

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,42 @@
 var db = require("../models");
 
 module.exports = function(app) {
-app.put("/bounties/accepted/:id", function(req,res){
-  console.log("Test Accepted");
-  db.Bounty.update({
-    accepted: true
-  },{
-    where:{
-      id: req.params.id
-    }
-  }).then(function(){
-    res.redirect("/bounties");
-  })
-});
+  app.put("/bounties/accepted/:id", function(req, res) {
+    console.log("Test Accepted");
+    db.Bounty.update(
+      {
+        accepted: true
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    ).then(function() {
+      res.redirect("/bounties");
+    });
+  });
 
-app.put("/bounties/completed/:id", function(req,res){
-  console.log("Test Completed");
-  db.Bounty.update({
-    completed: true
-  },{
-    where:{
-      id: req.params.id
-    }
-  }).then(function(){
-    res.redirect("/bounties");
-  })
-});
+  app.put("/bounties/completed/:id", function(req, res) {
+    console.log("Test Completed");
+    db.Bounty.update(
+      {
+        completed: true
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    ).then(function() {
+      res.redirect("/bounties");
+    });
+  });
 
-}
+  app.post("/bounties/delete/:id", function(req, res) {
+    console.log("Deleted Bounty");
+    db.Bounty.destroy({ where: { id: req.params.id } }).then(function() {
+      res.redirect("/bounties");
+    });
+  });
+};
