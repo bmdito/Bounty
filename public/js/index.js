a1e6e197988c19518be8749f264a2829faa125be
Jenn <jenn.a.martinez@outlook.com>
2019-04-09 18:30:00 -0700

    recent code

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,22 @@
 // eslint-disable-next-line no-empty-function
+var x = document.getElementById("myAudio");
+
+
 $(document).ready(function() {
+  function playAudio() {
+    var x = document.getElementById("myAudio");
+
+  x.play();
+  console.log("song");
+  }
 
   $(document).on("click", "button.accepted", toggleAccepted);
   
   
   function toggleAccepted(event) {
     console.log("BUTTON PRESSED");
-
   }
 
-  
+  playAudio();
   
 });
